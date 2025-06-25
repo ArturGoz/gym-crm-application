d5ae0b97cbb01b09e89c8c71eff59fdcffe1dec4
ArturGoz <arturrm4@gmail.com>
2025-06-25 15:31:45 +0300

    GCA-17 Create DAOs for data access operations - added common in-memory map storage

--- src/main/java/com/gca/storage/StorageConfig.java
+++ src/main/java/com/gca/storage/StorageConfig.java
@@ -3,29 +3,41 @@ package com.gca.storage;
 import com.gca.model.Trainee;
 import com.gca.model.Trainer;
 import com.gca.model.Training;
-import com.gca.model.TrainingType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-
 import java.util.HashMap;
 import java.util.Map;
+
 
 @Configuration
 @PropertySource(value = "classpath:application.yml", factory = YamlPropertySourceFactory.class)
 public class StorageConfig {
+    private static final Map<Namespace, Map<Long, ?>> storageMap;
+
+    static
+    {
+        storageMap = new HashMap<>();
+        storageMap.put(Namespace.TRAINING, new HashMap<Long, Training>());
+        storageMap.put(Namespace.TRAINER, new HashMap<Long, Trainer>());
+        storageMap.put(Namespace.TRAINEE, new HashMap<Long, Trainee>());
+    }
+
     @Bean
+    @SuppressWarnings("unchecked")
     public Map<Long, Training> trainingStorage() {
-        return new HashMap<>();
+        return (Map<Long, Training>) storageMap.get(Namespace.TRAINING);
     }
 
     @Bean
+    @SuppressWarnings("unchecked")
     public Map<Long, Trainer> trainerStorage() {
-        return new HashMap<>();
+        return (Map<Long, Trainer>) storageMap.get(Namespace.TRAINER);
     }
 
     @Bean
+    @SuppressWarnings("unchecked")
     public Map<Long, Trainee> traineeStorage() {
-        return new HashMap<>();
+        return (Map<Long, Trainee>) storageMap.get(Namespace.TRAINEE);
     }
 }
