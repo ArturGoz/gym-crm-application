db58016bddf0c41e61310af875a522f5269f7d2b
ArturGoz <arturrm4@gmail.com>
2025-06-30 17:06:04 +0300

    GCA-19 unit testing - added logg test

--- src/test/java/com/gca/service/common/UsernameGeneratorTest.java
+++ src/test/java/com/gca/service/common/UsernameGeneratorTest.java
@@ -1,5 +1,11 @@
 package com.gca.service.common;
 
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+
 import com.gca.dao.TraineeDAO;
 import com.gca.dao.TrainerDAO;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,8 +13,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
+import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -31,8 +40,8 @@ class UsernameGeneratorTest {
 
     @Test
     void generate_shouldReturnBaseNameIfNotExists() {
-        when(traineeDAO.getAllUsernames()).thenReturn(Collections.emptyList());
-        when(trainerDAO.getAllUsernames()).thenReturn(Collections.emptyList());
+        when(traineeDAO.getAllUsernames()).thenReturn(emptyList());
+        when(trainerDAO.getAllUsernames()).thenReturn(emptyList());
 
         String result = sut.generate("John", "Doe");
 
@@ -42,7 +51,7 @@ class UsernameGeneratorTest {
     @Test
     void generate_shouldReturnWithSuffixIfBaseTaken() {
         when(traineeDAO.getAllUsernames()).thenReturn(Arrays.asList("John.Doe"));
-        when(trainerDAO.getAllUsernames()).thenReturn(Collections.emptyList());
+        when(trainerDAO.getAllUsernames()).thenReturn(emptyList());
 
         String result = sut.generate("John", "Doe");
 
@@ -62,10 +71,30 @@ class UsernameGeneratorTest {
     @Test
     void generate_shouldBeCaseSensitive() {
         when(traineeDAO.getAllUsernames()).thenReturn(Collections.singletonList("john.doe"));
-        when(trainerDAO.getAllUsernames()).thenReturn(Collections.emptyList());
+        when(trainerDAO.getAllUsernames()).thenReturn(emptyList());
 
         String result = sut.generate("John", "Doe");
 
         assertEquals("John.Doe", result);
+    }
+
+    @Test
+    void generate_shouldLogIfUsernameExist() {
+        when(traineeDAO.getAllUsernames()).thenReturn(List.of("john.doe"));
+        when(trainerDAO.getAllUsernames()).thenReturn(emptyList());
+
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        ((Logger) LoggerFactory.getLogger(UsernameGenerator.class)).addAppender(listAppender);
+        listAppender.start();
+
+        sut.generate("john", "doe");
+
+        assertEquals(2, listAppender.list.size());
+
+        ILoggingEvent secondLog = listAppender.list.get(1);
+        assertEquals(Level.WARN, secondLog.getLevel());
+
+        String msg = secondLog.getFormattedMessage();
+        assertTrue(msg.equalsIgnoreCase("User with username john.doe already exists, generated alternative username: john.doe1"));
     }
 }
