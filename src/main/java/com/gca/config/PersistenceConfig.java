342812796f12bf5701d229a0ef8c75debfbf0ddb
ArturGoz <arturrm4@gmail.com>
2025-07-02 14:45:28 +0300

    GCA-27 Add Hibernate configuration - refactoring

--- src/main/java/com/gca/config/PersistenceConfig.java
+++ src/main/java/com/gca/config/PersistenceConfig.java
@@ -1,24 +1,33 @@
 package com.gca.config;
 
+import com.gca.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.apache.tomcat.jdbc.pool.DataSource;
 
-import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = "com.gca")
-@EnableTransactionManagement
 @PropertySource(value = "classpath:application.yml", factory = YamlPropertySourceFactory.class)
 public class PersistenceConfig {
+
+    private static final boolean SHOW_SQL = true;
+    private static final boolean FORMAT_SQL = true;
+
+    private static final String PROP_JDBC_DRIVER = "jakarta.persistence.jdbc.driver";
+    private static final String PROP_JDBC_URL = "jakarta.persistence.jdbc.url";
+    private static final String PROP_JDBC_USER = "jakarta.persistence.jdbc.user";
+    private static final String PROP_JDBC_PASSWORD = "jakarta.persistence.jdbc.password";
 
     @Value("${spring.datasource.url}")
     private String dbUrl;
@@ -40,7 +49,7 @@ public class PersistenceConfig {
 
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource ds = new DriverManagerDataSource();
+        DataSource ds = new DataSource();
         ds.setDriverClassName(dbDriver);
         ds.setUrl(dbUrl);
         ds.setUsername(dbUsername);
@@ -49,26 +58,31 @@ public class PersistenceConfig {
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setDataSource(dataSource());
-        emf.setPackagesToScan("com.example.model");
-        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+    public SessionFactory sessionFactory() {
+        Properties settings = hibernateProperties();
 
-        Properties jpaProperties = new Properties();
-        jpaProperties.put("hibernate.dialect", hibernateDialect);
-        jpaProperties.put("hibernate.hbm2ddl.auto", ddlAuto);
-        jpaProperties.put("hibernate.show_sql", "true");
-        jpaProperties.put("hibernate.format_sql", "true");
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(settings)
+                .build();
 
-        emf.setJpaProperties(jpaProperties);
-        return emf;
+        MetadataSources sources = new MetadataSources(serviceRegistry)
+                .addAnnotatedClass(User.class);
+
+        Metadata metadata = sources.buildMetadata();
+
+        return metadata.buildSessionFactory();
     }
 
-    @Bean
-    public JpaTransactionManager transactionManager() {
-        JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return txManager;
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put(PROP_JDBC_DRIVER, dbDriver);
+        properties.put(PROP_JDBC_URL, dbUrl);
+        properties.put(PROP_JDBC_USER, dbUsername);
+        properties.put(PROP_JDBC_PASSWORD, dbPassword);
+        properties.put(Environment.DIALECT, hibernateDialect);
+        properties.put(Environment.HBM2DDL_AUTO, ddlAuto);
+        properties.put(Environment.SHOW_SQL, String.valueOf(SHOW_SQL));
+        properties.put(Environment.FORMAT_SQL, String.valueOf(FORMAT_SQL));
+        return properties;
     }
 }
