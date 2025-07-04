8cda87c9283ee1f4516a27abc5e2e9e7d64b9567
ArturGoz <arturrm4@gmail.com>
2025-07-04 22:13:03 +0300

    GCA-35 Add Hibernate mapping for Entities - fixed dto for trainer/trainee

--- src/main/java/com/gca/dto/trainer/TrainerUpdateRequest.java
+++ src/main/java/com/gca/dto/trainer/TrainerUpdateRequest.java
@@ -11,6 +11,7 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TrainerUpdateRequest {
+    private Long id;
     private Long userId;
     private TrainingType specialization;
 }
