98fa469a816ae9012a28db696f4192cf99ba9a63
ArturGoz <arturrm4@gmail.com>
2025-07-04 12:36:19 +0300

    GCA-35 Add Hibernate mapping for Entities - added entities

--- src/main/java/com/gca/model/Trainer.java
+++ src/main/java/com/gca/model/Trainer.java
@@ -1,24 +1,56 @@
 package com.gca.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.experimental.SuperBuilder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@EqualsAndHashCode(callSuper = true)
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "trainers")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder(toBuilder = true)
 @Getter
-@SuperBuilder(toBuilder = true)
-public class Trainer extends User {
-    private final String specialization;
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Trainer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    @EqualsAndHashCode.Include
+    private Long id;
 
-    @Override
-    public String toString() {
-        return "Trainer{" +
-                "userId=" + getUserId() +
-                ", firstName='" + getFirstName() + '\'' +
-                ", lastName='" + getLastName() + '\'' +
-                ", username='" + getUsername() + '\'' +
-                ", isActive=" + isActive() +
-                ", specialization='" + getSpecialization() + '\'' +
-                '}';
-    }
+    @ToString.Exclude
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "specialization_id", nullable = false)
+    private TrainingType specialization;
+
+    @ToString.Exclude
+    @OneToOne(optional = false, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @ToString.Exclude
+    @Builder.Default
+    @ManyToMany(mappedBy = "trainers", fetch = FetchType.LAZY)
+    private Set<Trainee> trainees = new HashSet<>();
 }
