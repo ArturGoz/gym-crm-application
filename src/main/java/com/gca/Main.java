ae81ffb1f6c3699f870cb478e33765b61294200c
ArturGoz <arturrm4@gmail.com>
2025-07-06 13:52:46 +0300

    GCA-29 Migrate DAO to Hibernate - deleted test

--- src/main/java/com/gca/Main.java
+++ src/main/java/com/gca/Main.java
@@ -2,10 +2,6 @@ package com.gca;
 
 import com.gca.config.LiquibaseConfig;
 import com.gca.config.PersistenceConfig;
-import com.gca.dao.UserDAO;
-import com.gca.model.User;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
@@ -16,35 +12,6 @@ public class Main {
         context.register(PersistenceConfig.class, LiquibaseConfig.class);
         context.refresh();
 
-        SessionFactory sessionFactory = context.getBean(SessionFactory.class);
-        UserDAO userDAO = context.getBean(UserDAO.class);
-
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-
-        User user = User.builder()
-                .username("testuser")
-                .firstName("John")
-                .lastName("Doe")
-                .password("secret")
-                .isActive(true)
-                .build();
-
-        // Виклик CRUD
-        userDAO.delete(3L);
-        System.out.println("Created Trainee: " + user.getId());
-
-/*        // Read
-        User found = userDAO.getById(user.getId());
-        System.out.println("Found Trainee: " + found);
-
-        // Update
-        user.setIsActive(false);
-        userDAO.update(found);
-
-        // Delete
-        userDAO.delete(found.getId());*/
-        session.getTransaction().commit();
         context.close();
     }
 }
