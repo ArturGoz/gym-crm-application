d5fdef514fd92a219a2e5aa4113ca2af212115e5
ArturGoz <arturrm4@gmail.com>
2025-07-12 22:20:51 +0300

    GCA-34 Introduce transaction management for vital actions - delete test in main

--- src/main/java/com/gca/Main.java
+++ src/main/java/com/gca/Main.java
@@ -2,8 +2,6 @@ package com.gca;
 
 import com.gca.config.AppConfig;
 import com.gca.config.PersistenceConfig;
-import com.gca.dto.user.UserResponse;
-import com.gca.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
@@ -13,10 +11,6 @@ public class Main {
 
         context.register(AppConfig.class, PersistenceConfig.class);
         context.refresh();
-
-        UserService userService = context.getBean(UserService.class);
-        UserResponse userResponse = userService.getUserById(1L);
-        System.out.println(userResponse);
 
         context.close();
     }
