6f93b19d27a557f7f62ee3f49c8ec77fa782ee93
ArturGoz <arturrm4@gmail.com>
2025-07-16 17:45:37 +0300

    GCA-69 Prepare Error Handling PoC - refactoring

--- src/main/java/com/gca/exception/ErrorHandler.java
+++ src/main/java/com/gca/exception/ErrorHandler.java
@@ -1,63 +1,96 @@
 package com.gca.exception;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Set;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static com.gca.exception.ApiError.AUTHENTICATION_ERROR;
+import static com.gca.exception.ApiError.DATABASE_ERROR;
+import static com.gca.exception.ApiError.INVALID_REQUEST_ERROR;
+import static com.gca.exception.ApiError.NOT_FOUND_ERROR;
+import static com.gca.exception.ApiError.SERVER_ERROR;
+import static com.gca.exception.ApiError.VALIDATION_ERROR;
 
 @ControllerAdvice
 @Slf4j
 public class ErrorHandler {
-
-    private static final int STATUS_BAD_REQUEST = BAD_REQUEST.value();
-    private static final int STATUS_INTERNAL_SERVER_ERROR = INTERNAL_SERVER_ERROR.value();
+    private static final Set<String> BAD_REQUEST_PREFIXES = Set.of(
+            "Invalid trainee username"
+    );
 
     @ExceptionHandler(ServiceException.class)
-    public ResponseEntity<Map<String, Object>> handleServiceException(ServiceException ex) {
+    public ResponseEntity<ErrorResponse> handleServiceException(ServiceException ex) {
         log.error("ServiceException: {}", ex.getMessage(), ex);
+        ApiError error = resolveError(ex);
 
-        Map<String, Object> body = createResponseBody(
-                STATUS_BAD_REQUEST,
-                ex.getMessage()
-        );
-
-        return new ResponseEntity<>(body, BAD_REQUEST);
+        return buildErrorResponse(error);
     }
 
     @ExceptionHandler(DaoException.class)
-    public ResponseEntity<Map<String, Object>> handleDaoException(DaoException ex) {
-        log.error("DaoException: {}", ex.getMessage(), ex);
+    public ResponseEntity<ErrorResponse> handleDaoException(DaoException ex) {
+        log.error("Database Exception: {}", ex.getMessage(), ex);
 
-        Map<String, Object> body = createResponseBody(
-                STATUS_INTERNAL_SERVER_ERROR,
-                "Data Access Error"
-        );
-        return new ResponseEntity<>(body, INTERNAL_SERVER_ERROR);
+        return buildErrorResponse(DATABASE_ERROR);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundExceptions(Exception ex) {
+        log.error("Entity not found Exception: {}", ex.getMessage(), ex);
+
+        return buildErrorResponse(NOT_FOUND_ERROR, ex.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(Exception ex) {
+        log.error("Authentication Exception: {}", ex.getMessage(), ex);
+
+        return buildErrorResponse(VALIDATION_ERROR, ex.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<Map<String, Object>> handleAllOtherExceptions(RuntimeException ex) {
+    public ResponseEntity<ErrorResponse> handleUnhandledExceptions(Exception ex) {
         log.error("Unhandled Exception: {}", ex.getMessage(), ex);
 
-        Map<String, Object> body = createResponseBody(
-                STATUS_INTERNAL_SERVER_ERROR,
-                "Something went wrong."
-        );
-
-        return new ResponseEntity<>(body, INTERNAL_SERVER_ERROR);
+        return buildErrorResponse(SERVER_ERROR);
     }
 
-    private Map<String, Object> createResponseBody(int error, String message) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("errorCode", error);
-        body.put("errorMessage", message);
+    @ExceptionHandler(UserNotAuthenticatedException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotAuthenticatedExceptions(Exception ex) {
+        log.error("Authentication Exception: {}", ex.getMessage(), ex);
 
-        return body;
+        return buildErrorResponse(AUTHENTICATION_ERROR);
+    }
+
+    private ResponseEntity<ErrorResponse> buildErrorResponse(ApiError apiError) {
+        return buildErrorResponse(apiError, null);
+
+    }
+
+    private ResponseEntity<ErrorResponse> buildErrorResponse(ApiError apiError, String message) {
+        message = StringUtils.isBlank(message) ? "" : message;
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorCode(String.valueOf(apiError.getCode()))
+                .errorMessage(apiError.getMessage() + message)
+                .build();
+
+        return new ResponseEntity<>(errorResponse, apiError.getHttpStatus());
+    }
+
+    private ApiError resolveError(ServiceException ex) {
+        String normalizedMessage = StringUtils.isBlank(ex.getMessage()) ? "" : ex.getMessage().toLowerCase();
+
+        return BAD_REQUEST_PREFIXES.stream()
+                .map(String::toLowerCase)
+                .filter(normalizedMessage::startsWith)
+                .findFirst()
+                .map(suffix -> INVALID_REQUEST_ERROR)
+                .orElse(SERVER_ERROR);
     }
 }
--- src/test/java/com/gca/exception/ErrorHandlerTest.java
+++ src/test/java/com/gca/exception/ErrorHandlerTest.java
@@ -1,20 +1,26 @@
 package com.gca.exception;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Map;
-
+import static com.gca.exception.ApiError.AUTHENTICATION_ERROR;
+import static com.gca.exception.ApiError.DATABASE_ERROR;
+import static com.gca.exception.ApiError.INVALID_REQUEST_ERROR;
+import static com.gca.exception.ApiError.NOT_FOUND_ERROR;
+import static com.gca.exception.ApiError.SERVER_ERROR;
+import static com.gca.exception.ApiError.VALIDATION_ERROR;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 class ErrorHandlerTest {
-
-    private static final int STATUS_BAD_REQUEST = BAD_REQUEST.value();
-    private static final int STATUS_INTERNAL_SERVER_ERROR = INTERNAL_SERVER_ERROR.value();
 
     private ErrorHandler errorHandler;
 
@@ -24,47 +30,114 @@ class ErrorHandlerTest {
     }
 
     @Test
-    void handleServiceException_shouldReturnBadRequestWithProperBody() {
-        String errorMessage = "Something went wrong";
-        ServiceException exception = new ServiceException(errorMessage);
+    void handleServiceException_shouldReturnInvalidRequestError_whenPrefixMatches() {
+        String message = "Invalid trainee username format is wrong";
+        ServiceException ex = new ServiceException(message);
 
-        ResponseEntity<Map<String, Object>> response = errorHandler.handleServiceException(exception);
+        ResponseEntity<ErrorResponse> response = errorHandler.handleServiceException(ex);
 
         assertEquals(BAD_REQUEST, response.getStatusCode());
 
-        Map<String, Object> body = response.getBody();
+        ErrorResponse body = response.getBody();
 
         assertNotNull(body);
-        assertEquals(STATUS_BAD_REQUEST, body.get("errorCode"));
-        assertEquals(errorMessage, body.get("errorMessage"));
+        assertEquals(String.valueOf(INVALID_REQUEST_ERROR.getCode()), body.getErrorCode());
+        assertTrue(body.getErrorMessage().contains(INVALID_REQUEST_ERROR.getMessage()));
     }
 
     @Test
-    void handleDaoException_shouldReturnInternalServerErrorWithProperBody() {
-        String daoErrorMessage = "Database down";
-        DaoException exception = new DaoException(daoErrorMessage);
+    void handleServiceException_shouldReturnServerError_whenPrefixDoesNotMatch() {
+        String message = "Some unknown service exception";
+        ServiceException ex = new ServiceException(message);
 
-        ResponseEntity<Map<String, Object>> response = errorHandler.handleDaoException(exception);
+        ResponseEntity<ErrorResponse> response = errorHandler.handleServiceException(ex);
 
         assertEquals(INTERNAL_SERVER_ERROR, response.getStatusCode());
 
-        Map<String, Object> body = response.getBody();
+        ErrorResponse body = response.getBody();
+
         assertNotNull(body);
-        assertEquals(STATUS_INTERNAL_SERVER_ERROR, body.get("errorCode"));
-        assertEquals("Data Access Error", body.get("errorMessage"));
+        assertEquals(String.valueOf(SERVER_ERROR.getCode()), body.getErrorCode());
+        assertEquals(SERVER_ERROR.getMessage(), body.getErrorMessage());
     }
 
     @Test
-    void handleAllOtherExceptions_shouldReturnInternalServerErrorWithGenericMessage() {
-        RuntimeException exception = new RuntimeException("Unexpected failure");
+    void handleDaoException_shouldReturnDatabaseError() {
+        DaoException ex = new DaoException("DB failure");
 
-        ResponseEntity<Map<String, Object>> response = errorHandler.handleAllOtherExceptions(exception);
+        ResponseEntity<ErrorResponse> response = errorHandler.handleDaoException(ex);
 
         assertEquals(INTERNAL_SERVER_ERROR, response.getStatusCode());
 
-        Map<String, Object> body = response.getBody();
+        ErrorResponse body = response.getBody();
+
         assertNotNull(body);
-        assertEquals(STATUS_INTERNAL_SERVER_ERROR, body.get("errorCode"));
-        assertEquals("Something went wrong.", body.get("errorMessage"));
+        assertEquals(String.valueOf(DATABASE_ERROR.getCode()), body.getErrorCode());
+        assertEquals(DATABASE_ERROR.getMessage(), body.getErrorMessage());
     }
+
+    @Test
+    void handleEntityNotFoundExceptions_shouldReturnNotFoundErrorWithMessage() {
+        String message = "Trainee with ID 5 not found";
+        EntityNotFoundException ex = new EntityNotFoundException(message);
+
+        ResponseEntity<ErrorResponse> response = errorHandler.handleEntityNotFoundExceptions(ex);
+
+        assertEquals(NOT_FOUND, response.getStatusCode());
+
+        ErrorResponse body = response.getBody();
+
+        assertNotNull(body);
+        assertEquals(String.valueOf(NOT_FOUND_ERROR.getCode()), body.getErrorCode());
+        assertTrue(body.getErrorMessage().contains(NOT_FOUND_ERROR.getMessage()));
+        assertTrue(body.getErrorMessage().contains(message));
+    }
+
+    @Test
+    void handleValidationExceptions_shouldReturnValidationErrorWithMessage() {
+        String message = "Field must not be blank";
+        ConstraintViolationException ex = new ConstraintViolationException(message, null);
+
+        ResponseEntity<ErrorResponse> response = errorHandler.handleValidationExceptions(ex);
+
+        assertEquals(BAD_REQUEST, response.getStatusCode());
+
+        ErrorResponse body = response.getBody();
+
+        assertNotNull(body);
+        assertEquals(String.valueOf(VALIDATION_ERROR.getCode()), body.getErrorCode());
+        assertTrue(body.getErrorMessage().contains(VALIDATION_ERROR.getMessage()));
+        assertTrue(body.getErrorMessage().contains(message));
+    }
+
+    @Test
+    void handleUserNotAuthenticatedExceptions_shouldReturnAuthenticationError() {
+        UserNotAuthenticatedException ex = new UserNotAuthenticatedException("No token");
+
+        ResponseEntity<ErrorResponse> response = errorHandler.handleUserNotAuthenticatedExceptions(ex);
+
+        assertEquals(UNAUTHORIZED, response.getStatusCode());
+
+        ErrorResponse body = response.getBody();
+
+        assertNotNull(body);
+        assertEquals(String.valueOf(AUTHENTICATION_ERROR.getCode()), body.getErrorCode());
+        assertEquals(AUTHENTICATION_ERROR.getMessage(), body.getErrorMessage());
+    }
+
+    @Test
+    void handleUnhandledExceptions_shouldReturnServerError() {
+        RuntimeException ex = new RuntimeException("Unknown runtime issue");
+
+        ResponseEntity<ErrorResponse> response = errorHandler.handleUnhandledExceptions(ex);
+
+        assertEquals(INTERNAL_SERVER_ERROR, response.getStatusCode());
+
+        ErrorResponse body = response.getBody();
+
+        assertNotNull(body);
+        assertEquals(String.valueOf(SERVER_ERROR.getCode()), body.getErrorCode());
+        assertEquals(SERVER_ERROR.getMessage(), body.getErrorMessage());
+    }
+
 }
