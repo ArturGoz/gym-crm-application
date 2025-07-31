fefb99f4d55883a4fe3224be1da8072432d03183
ArturGoz <arturrm4@gmail.com>
2025-07-31 12:27:25 +0300

    GCA-119 Migrate to Spring Boot - removed unnecessary classes

--- src/main/java/com/gca/config/AppConfig.java
+++ src/main/java/com/gca/config/AppConfig.java
@@ -1,26 +1,15 @@
 package com.gca.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import com.gca.security.CachingRequestResponseFilter;
+import com.gca.security.TransactionIdFilter;
+import jakarta.servlet.DispatcherType;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @Configuration
-@ComponentScan(basePackages = {
-        "com.gca.dao",
-        "com.gca.service",
-        "com.gca.facade",
-        "com.gca.mapper",
-        "com.gca.security",
-        "com.gca.exception"
-})
 @EnableAspectJAutoProxy
 public class AppConfig {
     @Bean
@@ -29,24 +18,21 @@ public class AppConfig {
     }
 
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        return mapper;
+    public FilterRegistrationBean<CachingRequestResponseFilter> cachingFilter() {
+        FilterRegistrationBean<CachingRequestResponseFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CachingRequestResponseFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setDispatcherTypes(DispatcherType.REQUEST);
+        registrationBean.setAsyncSupported(true);
+        return registrationBean;
     }
 
     @Bean
-    public MethodValidationPostProcessor methodValidationPostProcessor() {
-        return new MethodValidationPostProcessor();
-    }
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("My Spring MVC API")
-                        .version("1.0.0")
-                        .description("API documentation for Spring MVC project"));
+    public FilterRegistrationBean<TransactionIdFilter> transactionIdFilter() {
+        FilterRegistrationBean<TransactionIdFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new TransactionIdFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setAsyncSupported(true);
+        return registrationBean;
     }
 }
